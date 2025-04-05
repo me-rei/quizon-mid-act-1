12356fd4facb8eb985ddda1222e287c41f9c812c
me-rei <elizaquizon71@gmail.com>
2025-04-05 10:59:55 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCj1Dk-zlp2jaj1Ec0QHLkyRevf6LTp7ZU",
+  authDomain: "quizon-midterms-act.firebaseapp.com",
+  databaseURL: "https://quizon-midterms-act-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "quizon-midterms-act",
+  storageBucket: "quizon-midterms-act.firebasestorage.app",
+  messagingSenderId: "175898339675",
+  appId: "1:175898339675:web:98bcc382aebc56567c927e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ myscript.js
@@ -0,0 +1,88 @@
+var rollV, nameV, genderV, addressV, emailV, birthdateV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  emailV = document.getElementById("email").value;
+  birthdateV = document.getElementById("birthdate").value;
+  console.log(rollV, nameV, addressV, genderV, emailV, birthdateV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      email: emailV,
+      birthdate: birthdateV,
+    });
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("birthdate").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+      document.getElementById("email").value = snap.val().email;
+      document.getElementById("birthdate").value = snap.val().birthdate;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      email: emailV,
+      birthdate: birthdateV,
+    });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("birthdate").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("birthdate").value = "";
+};
